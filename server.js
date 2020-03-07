78a1f3235bfdcf6adb095d271d32074986ab8998
Sebas <seplarui@gmail.com>
2020-03-07 11:33:56 +0100

    server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(cors(corsOptions))
 
 app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
     res.json({ message: 'Home page' })
